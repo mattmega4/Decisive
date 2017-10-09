d7580d1e44f170df5891276db8d145c0d003d559
mattmega4 <mattmega4@gmail.com>
2017-10-08 20:25:22 -0700

    restarted

---
+++ Decisive/Controller/LandingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LandingViewController.swift
+//  Decisive
+//
+//  Created by Matthew Howes Singleton on 10/8/17.
+//  Copyright © 2017 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
